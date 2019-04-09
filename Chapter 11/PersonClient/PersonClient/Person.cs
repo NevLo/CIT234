2fa64d2e2d4af041bfa74e0579b848b668202ee0
NevLo <pilleychristian@gmail.com>
2019-04-08 18:27:29 -0700

    Added Chapter 11 Pre-HW

---
+++ Chapter 11/PersonClient/PersonClient/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonClient
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsMale { get; set; }
+
+        public Person()
+        {
+            Name = "";
+            Age = 0;
+            IsMale = true;
+        }
+        public Person(string n, int a, bool m)
+        {
+            Name = n;
+            Age = a;
+            IsMale = m;
+        }
+
+        public string getNickName()
+        {
+            string nick = "";
+            int year = DateTime.Today.Year;
+            int birthYear = year - Age;
+            if(birthYear > 2000)
+            {
+                nick = "Broski_" + Name;
+            }
+            else if(birthYear > 1990)
+            {
+                nick = "Homie_" + Name;
+            }else if(birthYear > 1980)
+            {
+                nick = "Dude_" + Name;
+            }
+            else
+            {
+                nick = "Sir_" + Name;
+            }
+
+            return nick;
+        }
+
+        public int getBirthYear()
+        {
+            return DateTime.Now.Year - Age;
+        }
+
+        public int[] getShirtColor()
+        {
+            Random rand = new Random();
+            int x = rand.Next(256);
+            int y = rand.Next(256);
+            int z = rand.Next(256);
+
+
+            int[] shirtColor = {x,y,z};
+
+
+            return shirtColor;
+        }
+        
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Chapter 11/PersonClient/PersonClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PersonClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            Person b = new Person("Katalina", 28,false);
+
+            Console.WriteLine(b.getNickName());
+            Console.WriteLine(b.getBirthYear());
+            int[] sc = b.getShirtColor();
+            Console.WriteLine($"Shirt color is <{sc[0]},{sc[1]},{sc[2]}>");
+
+        }
+    }
+}
