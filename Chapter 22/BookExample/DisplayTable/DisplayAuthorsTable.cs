280eaf59440406f2c7ddb4e34e4dec316df175a5
NevLo <pilleychristian@gmail.com>
2019-04-24 19:54:46 -0700

    Added Chapter 22 Inclass Work

---
+++ Chapter 22/BookExample/DisplayTable/DisplayAuthorsTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookExample;
+
+namespace DisplayTable
+{
+    public partial class DisplayAuthorsTable : Form
+    {
+        private BooksEntities dbContext = new BooksEntities(); 
+
+
+        public DisplayAuthorsTable()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayAuthorsTable_Load(object sender, EventArgs e)
+        {
+            dbContext.Authors
+                .OrderBy(author => author.LastName)
+                .ThenBy(author => author.FirstName)
+                .Load();
+            authorBindingSource.DataSource = dbContext.Authors.Local;
+
+        }
+
+        private void authorBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            Validate();
+            authorBindingSource.EndEdit();
+
+            try
+            {
+                dbContext.SaveChanges();
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Chapter 22/BookExample/QueryResult/DisplayTitleResult.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookExample;
+namespace QueryResult
+{
+    public partial class DisplayTitleResult : Form
+    {
+        private BooksEntities dbContext = new BooksEntities();
+
+
+        public DisplayTitleResult()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayTitleResult_Load(object sender, EventArgs e)
+        {
+            dbContext.Titles.Load();
+
+            QuerysComboBox.SelectedIndex = 0;
+        }
+
+        private void QuerysComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (QuerysComboBox.SelectedIndex)
+            {
+                case 0:
+                    titleBindingSource.DataSource =
+                        dbContext.Titles.Local
+                        .OrderBy(t => t.Title1);
+                    break;
+                case 1:
+                    titleBindingSource.DataSource =
+                        dbContext.Titles.Local
+                        .Where(t => t.Copyright.Equals("2014"))
+                        .OrderBy(t => t.Title1);
+                    break;
+                case 2:
+                    titleBindingSource.DataSource =
+                        dbContext.Titles.Local
+                        .Where(t => t.Title1.EndsWith("How to Program"))
+                        .OrderByDescending(t => t.Title1);
+                    break;
+                default:
+                    break;
+
+
+            }
+        }
+    }
+}
